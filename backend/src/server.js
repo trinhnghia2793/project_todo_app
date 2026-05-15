d95d868685ac39c64077edc66e9fc8f3baa6891f
trinhnghia2793 <trinhnghia270903@gmail.com>
2026-05-15 13:39:18 +0700

    Deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,32 @@ import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
 import taskRoute from './routes/tasksRouters.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 const app = express();
 
 // middlewares
 app.use(express.json()); // middleware: check if data is json --> change to object 
-app.use(cors({ origin: "http://localhost:5173" }));
+
+// development middleware
+if(process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 app.use("/api/tasks", taskRoute);
+
+// production middleware
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // get code from frontend/dist
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
